0a61450d92a9f67069e1c2e09201fcf7e5f49672
Methma Peiris <methma34@gmail.com>
2021-12-09 15:39:21 +0530

    example for conditional rendering using element variables

--- conditional-rendering/src/components/UserGreeting.js
+++ conditional-rendering/src/components/UserGreeting.js
@@ -6,20 +6,36 @@ class UserGreeting extends Component {
         super(props)
 
         this.state = {
-            isLoggedIn: true //Change the value of isLoggedIn state attribute to change the message of the component
+            isLoggedIn: false //Change the value of isLoggedIn state attribute to change the message of the component
         }
     }
     render() {
+        /**
+         * Using element variables to implement conditional rendering
+         */
+        let greetingMessage
         if (this.state.isLoggedIn) {
-            return(
-                <div>Welcome Methma !</div>
-            )
+            greetingMessage = <div>Welcome Methma !</div>
         }
         else{
-            return (
-                <div>Welcome Guest !</div>
-            )
+            greetingMessage = <div>Welcome Guest !</div>
         }
+
+        return <div>{greetingMessage}</div>
+
+        /**
+         * Using if/else to implement conditional rendering
+         */
+        // if (this.state.isLoggedIn) {
+        //     return(
+        //         <div>Welcome Methma !</div>
+        //     )
+        // }
+        // else{
+        //     return (
+        //         <div>Welcome Guest !</div>
+        //     )
+        // }
     }
 }
 
