1f8ee62af62e962bb2caf9d4ef6e44dbde2fecb9
Methma Peiris <methma34@gmail.com>
2022-01-05 17:50:29 +0530

    Create User modal class

---
+++ reusable-modal/src/modals/User.tsx
@@ -0,0 +1,9 @@
+export default class User{
+    name: string;
+    userName: string;
+
+    constructor(name: string, userName:string) {
+        this.name = name;
+        this.userName = userName;
+    }
+}
