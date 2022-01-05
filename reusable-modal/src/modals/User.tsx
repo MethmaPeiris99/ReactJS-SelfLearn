eec82c4930d3a20a6f927dc3730210b837ce554a
Methma Peiris <methma34@gmail.com>
2022-01-05 19:22:03 +0530

    Remove unwanted files

--- reusable-modal/src/modals/User.tsx
+++
@@ -1,9 +0,0 @@
-export default class User{
-    name: string;
-    userName: string;
-
-    constructor(name: string, userName:string) {
-        this.name = name;
-        this.userName = userName;
-    }
-}
