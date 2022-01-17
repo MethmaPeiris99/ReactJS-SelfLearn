ce231d5161bc963d6199bd7f1492fde7e255377f
Methma Peiris <methma34@gmail.com>
2022-01-17 18:13:15 +0530

    Render multiple elements using React.createElement() example using a list as type

--- learning-reactjs-course/src/index.js
+++ learning-reactjs-course/src/index.js
@@ -5,7 +5,11 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  React.createElement("div", {style:{color:'blue'}}, React.createElement("h1",null, "Hi")),
+  React.createElement("div", {style:{color:'blue'}},
+      React.createElement("li",null, "Hot Dogs"),
+      React.createElement("li",null, "Hamburgers"),
+      React.createElement("li",null, "Pizza"),
+      React.createElement("li",null, "Pasta")),
   document.getElementById('root')
 );
 
