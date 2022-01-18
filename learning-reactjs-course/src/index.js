2eb9b31bde73ebe6289424459d9aee89bab1211d
Methma Peiris <methma34@gmail.com>
2022-01-18 10:34:13 +0530

    Example for rendering lists

--- learning-reactjs-course/src/index.js
+++ learning-reactjs-course/src/index.js
@@ -4,32 +4,21 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 
-let city = "Colombo";
+const listOfFrameworks = ["React","Angular","Springboot"]
 
-let person = {
-    name : "Methma",
-    age : 22
-}
-
-function App(){
-    return(<div>
-        <Hello library="React" message="Have fun"/>
-        <Hello library="Angular" message="Have fun"/>
-        <Hello library="Springboot" message="Have fun"/>
-    </div>);
-}
-function Hello({library, message}){
-    return (
-        <div>
-            <h1>Welcome to {library}!</h1>
-            <p>{message}</p>
-        </div>
+function App({frameworks}){
+    return(
+      <ul>
+          {frameworks.map(framework =>(
+              <li>{framework}</li>
+          ))}
+      </ul>
     );
 }
 
 ReactDOM.render(
     <div>
-        <App/>
+        <App frameworks={listOfFrameworks}/>
     </div>,
   document.getElementById('root')
 );
