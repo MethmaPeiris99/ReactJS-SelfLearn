0262e4ca164f324240f6f9669e8d249853fc12d6
Methma Peiris <methma34@gmail.com>
2022-01-18 10:58:25 +0530

    Example for rendering list of objects

--- learning-reactjs-course/src/index.js
+++ learning-reactjs-course/src/index.js
@@ -4,13 +4,20 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 
-const listOfFrameworks = ["React","Angular","Springboot"]
+const listOfFrameworks = [
+    {id: "1", name: "React", version: 4.5},
+    {id: "2", name: "Angular", version: 5.7},
+    {id: "3", name: "Springboot", version: 9.0}
+];
 
 function App({frameworks}){
     return(
       <ul>
           {frameworks.map(framework =>(
-              <li>{framework}</li>
+              <div>
+                  <h2>{framework.name}</h2>
+                  <h4>Version: {framework.version}</h4>
+              </div>
           ))}
       </ul>
     );
