16fd2907b7bfc3d79fd7009d572d1175aa2051dc
Methma Peiris <methma34@gmail.com>
2022-01-19 16:49:49 +0530

    Example for using multiple state variables

--- learning-reactjs-course/src/index.js
+++ learning-reactjs-course/src/index.js
@@ -5,26 +5,44 @@ import reportWebVitals from './reportWebVitals';
 
 function App(){
     const [status, setStatus] = useState("Open");
+    const [manager, setManager] = useState("Jon Snow");
+    const [year, setYear] = useState(2021);
 
     return(
-        <div>
-            <h1>Status: {status}</h1>
-            <button
-                onClick={() => setStatus("Open")}>
-                Open
-            </button>
-            <button
-                onClick={() => setStatus("Closed")}>
-                Closed
-            </button>
-            <button
-                onClick={() => setStatus("Back in 5 minutes")}>
-                Break
-            </button>
-        </div>
+        <>
+            <div>
+                <h1>Status: {status}</h1>
+                <button
+                    onClick={() => setStatus("Open")}>
+                    Open
+                </button>
+                <button
+                    onClick={() => setStatus("Closed")}>
+                    Closed
+                </button>
+                <button
+                    onClick={() => setStatus("Back in 5 minutes")}>
+                    Break
+                </button>
+            </div>
+            <div>
+                <h1>Manager on duty: {manager}</h1>
+                <button
+                    onClick={()=> setManager("Sansa Stark")}>
+                    New Manager
+                </button>
+            </div>
+
+            <div>
+                <h3>Year: {year}</h3>
+                <button
+                    onClick={() => setYear(year+1)}>
+                    New Year !
+                </button>
+            </div>
+        </>
     );
 }
-
 
 ReactDOM.render(
     <div>
