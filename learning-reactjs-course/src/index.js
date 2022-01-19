56c05a2f0099b7dd953da4e172d599a08cce6bb2
Methma Peiris <methma34@gmail.com>
2022-01-19 17:23:48 +0530

    Example for using useEffect

--- learning-reactjs-course/src/index.js
+++ learning-reactjs-course/src/index.js
@@ -1,52 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function App(){
-    const [status, setStatus] = useState("Open");
-    const [manager, setManager] = useState("Jon Snow");
-    const [year, setYear] = useState(2021);
+function Checkbox(){
+    const [checked, setChecked] = useState(false);
+
+    useEffect(()=>{
+        alert(`checked: ${checked.toString()}`);
+    });
 
     return(
         <>
-            <div>
-                <h1>Status: {status}</h1>
-                <button
-                    onClick={() => setStatus("Open")}>
-                    Open
-                </button>
-                <button
-                    onClick={() => setStatus("Closed")}>
-                    Closed
-                </button>
-                <button
-                    onClick={() => setStatus("Back in 5 minutes")}>
-                    Break
-                </button>
-            </div>
-            <div>
-                <h1>Manager on duty: {manager}</h1>
-                <button
-                    onClick={()=> setManager("Sansa Stark")}>
-                    New Manager
-                </button>
-            </div>
-
-            <div>
-                <h3>Year: {year}</h3>
-                <button
-                    onClick={() => setYear(year+1)}>
-                    New Year !
-                </button>
-            </div>
+            <input
+                type="checkbox"
+                value={checked}
+                onChange={() => setChecked(checked => !checked)}
+            />
+            {checked ? "Checked" : "Not checked"}
         </>
     );
 }
 
 ReactDOM.render(
     <div>
-        <App/>
+        <Checkbox/>
     </div>,
   document.getElementById('root')
 );
